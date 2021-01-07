edb7ccb06f5083754c7778e56b7776e796997e5e
nguyenquocanh-1724801030191 <50696011+nguyenquocanh-1724801030191@users.noreply.github.com>
2021-01-07 22:00:32 +0700

    first commit

---
+++ EmployeeRegisterAPI/EmployeeRegisterAPI/Models/EmployeeModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+
+namespace EmployeeRegisterAPI.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string EmployeeName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Occupation { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string ImageName { get; set; }
+
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+
+        [NotMapped]
+        public string ImageSrc { get; set; }
+
+        //Pascal(EmployeeName) -> Camel EmployeeID ->employeeID
+        //Camel(employeeName) -> Pascal
+    }
+}
